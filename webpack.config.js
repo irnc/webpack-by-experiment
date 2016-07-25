0361d20ee9376afabaf2c44bdf3d589313433d05
Pavel Zubkou <pavel.zubkou@instinctools.ru>
2016-07-25 18:31:13 +0300

    Add ProvidePlugin to webpack config
    At this stage `npm run build` will not change bundle.js, as $ was not used.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
+const webpack = require('webpack');
+
 module.exports = {
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
     path: './built',
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+    }),
+  ],
 };
